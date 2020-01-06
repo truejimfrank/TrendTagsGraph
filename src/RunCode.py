f336a197e64e74498eefddf9ee1b88f88656a6b6
truejimfrank <james.a.frank@hotmail.com>
2020-01-06 15:18:25 -0700

    created folders and scripts

---
+++ src/RunCode.py
@@ -0,0 +1,4 @@
+# A place to call on data and functions to get RESULTS!
+
+if __name__ == '__main__':
+    pass
---
+++ src/TrendClass.py
@@ -0,0 +1,11 @@
+# Class with functions for investigating YouTube Trend data
+
+class Trends(object):
+    def __init__(self):
+        pass
+
+    def load(self):
+        pass
+
+    def join(self):
+        pass
