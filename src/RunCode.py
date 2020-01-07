66d447385b91869215f7313db2c6e903ce923a9b
truejimfrank <james.a.frank@hotmail.com>
2020-01-07 13:59:13 -0700

    column reduce function

--- src/RunCode.py
+++ src/RunCode.py
@@ -8,8 +8,25 @@ from TrendClass import CleanData
 from TrendClass import TagsResearch
 
 if __name__ == '__main__':
+# filepath variables
     us_csv_path = "../data/trends/USvideos.csv"
     us_json_path = "../data/trends/US_category_id.json"
+# load data to dataframes
     df_us = CleanData.load_csv(us_csv_path)
     df_us_json = CleanData.load_json(us_json_path)
-    print(df_us.head())
+    # print(df_us.head())
+    # print(df_us_json.head())
+# data cleaning pipeline
+    CleanData.fill_nan(df_us)
+    # print(df_us.info())
+    CleanData.join_categories(df_us, df_us_json)
+    # print(df_us['category_name'].head(10))
+    CleanData.date_format(df_us)
+    # ["trend_end", "post_time"]
+    # df_view = pd.DataFrame(df_us["trend_end"].unique())
+    # print(df_view.sort_values(0, ascending=False).head(3))
+    # print(df_us["trend_end"].dtype)
+    # print(df_us["post_time"].dtype)
+# groupby video_id, select columns, make df copy
+    df_us_vid = CleanData.unique_reduce(df_us)
+    print(df_us_vid.columns)
--- src/TrendClass.py
+++ src/TrendClass.py
@@ -11,30 +11,50 @@ class CleanData(object):
     # def __init__(self):
     #     pass
 
-    def load_csv(self, file_path):
+    # This didn't work as intended
+    # def load_csv(self, file_path):
+    #     return pd.read_csv(file_path)
+
+    def load_csv(file_path):
         return pd.read_csv(file_path)
 
-    def load_json(self, file_path):
+    def load_json(file_path):
         return pd.read_json(file_path)
 
-    def fill_nan(self, df):
+    def fill_nan(df):
         df["description"] = df["description"].fillna(value="")
         return df
 
-    def join(self):
-    # join the category names from json files
-        pass
+    def join_categories(df, df_json):
+    # create category_name column from json files
+        cat_dict = {}
+        for cat in df_json["items"]:
+            cat_dict[int(cat["id"])] = cat["snippet"]["title"]
+        df['category_name'] = df['category_id'].map(cat_dict)
+        return df
 
-    def date_format(self):
+    def date_format(df):
     # ["trending_date", "publish_time"]
-    # 
-        pass
+    # ["trend_end", "post_time"]
+        df["trend_end"] = pd.to_datetime(df["trending_date"], format="%y.%d.%m")
+        df["post_time"] = pd.to_datetime(df["publish_time"])
+        return df
 
-    def unique_reduce(self):
-    # reduce df to most recent unique video data
+    def unique_reduce(df):
     # count # of days video appears on trending
+        day_max = df[['video_id', 'trend_end']].groupby(['video_id']).max().iloc[:,0]
+        day_min = df[['video_id', 'trend_end']].groupby(['video_id']).min().iloc[:,0]
+        df["trend_begin"] = df["video_id"].map(day_min)
+        delta_map = day_max - day_min + dt.timedelta(days=1)
+        df["days_trending"] = df["video_id"].map(delta_map)
     # record initial viewcount on trending start
-        pass
+        min_map = df[['video_id', 'views']].groupby(['video_id']).min().iloc[:,0]
+        df["views_initial"] = df["video_id"].map(min_map)
+    # reduce df to most recent unique video data
+        col_sel = ['video_id', 'title', 'channel_title', 'category_name', 'tags',
+                 'views', 'views_initial','trend_begin', 'trend_end', 'days_trending',
+                  'post_time', 'likes', 'dislikes', 'comment_count']
+        return df[col_sel].groupby(['video_id']).max().copy()
 
 
 class TagsResearch(object):
