176e1bc63dd0bea3a2c310a43800962d555aaccb
truejimfrank <james.a.frank@hotmail.com>
2020-01-06 17:11:21 -0700

    EDA categories

--- src/TrendClass.py
+++ src/TrendClass.py
@@ -4,8 +4,16 @@ class Trends(object):
     def __init__(self):
         pass
 
+    # load csv and json data into dataframes
     def load(self):
         pass
 
+    # join the category names from json files
     def join(self):
+        pass
+
+    # reduce df to most recent unique video data
+    # count # of times video appears on trending
+    # record initial viewcount on trending start
+    def unique(self):
         pass
