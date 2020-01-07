0852b5f091feba703e0863ce1cd040ef64c2da8e
truejimfrank <james.a.frank@hotmail.com>
2020-01-07 10:45:38 -0700

    script classes outline and start

--- src/RunCode.py
+++ src/RunCode.py
@@ -1,4 +1,15 @@
 # A place to call on data and functions to get RESULTS!
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import datetime as dt
+from TrendClass import CleanData
+from TrendClass import TagsResearch
 
 if __name__ == '__main__':
-    pass
+    us_csv_path = "../data/trends/USvideos.csv"
+    us_json_path = "../data/trends/US_category_id.json"
+    df_us = CleanData.load_csv(us_csv_path)
+    df_us_json = CleanData.load_json(us_json_path)
+    print(df_us.head())
--- src/TrendClass.py
+++ src/TrendClass.py
@@ -1,19 +1,48 @@
-# Class with functions for investigating YouTube Trend data
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+import datetime as dt
 
-class Trends(object):
-    def __init__(self):
-        pass
+class CleanData(object):
+    ''' Class with functions for manipulating trending video 
+    YouTube data.
+    '''
+    # def __init__(self):
+    #     pass
 
-    # load csv and json data into dataframes
-    def load(self):
-        pass
+    def load_csv(self, file_path):
+        return pd.read_csv(file_path)
 
-    # join the category names from json files
+    def load_json(self, file_path):
+        return pd.read_json(file_path)
+
+    def fill_nan(self, df):
+        df["description"] = df["description"].fillna(value="")
+        return df
+
     def join(self):
+    # join the category names from json files
         pass
 
+    def date_format(self):
+    # ["trending_date", "publish_time"]
+    # 
+        pass
+
+    def unique_reduce(self):
     # reduce df to most recent unique video data
-    # count # of times video appears on trending
+    # count # of days video appears on trending
     # record initial viewcount on trending start
-    def unique(self):
+        pass
+
+
+class TagsResearch(object):
+    ''' Tag formatting and synthesizing data from the tags column.
+    '''
+    def split_tags(self, tags_str):
+        return tags_str.replace('"', '').split('|')
+
+    def tags_format(self):
+    # transform text string into pd series
         pass
