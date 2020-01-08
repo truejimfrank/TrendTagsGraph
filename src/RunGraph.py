0e772dd430dee0b471d539953623e187c41bec9b
truejimfrank <james.a.frank@hotmail.com>
2020-01-08 12:26:45 -0700

    wordcloud graphing file

--- src/RunGraph.py
+++ src/RunGraph.py
@@ -8,12 +8,17 @@ from TagClass import TagsResearch
 from RunCode import run_pipeline
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 
+def word_cloud_image(big_string, file_path):
+    # other colormaps default=viridis, cubehelix, CMRmap
+    cloud = WordCloud(max_font_size=40, max_words=80, background_color="white", 
+                        colormap="CMRmap").generate(big_string)
+    cloud.to_file(file_path)
+
 def plot_barh():
     plt.figure(figsize=(15,10))
-    country.size().sort_values(ascending=False).plot.bar()
     plt.xticks(rotation=50)
-    plt.xlabel("Country of Origin")
-    plt.ylabel("Number of Wines")
+    plt.xlabel("X Label")
+    plt.ylabel("Y Label")
     plt.show()
 
 if __name__ == '__main__':
@@ -23,17 +28,16 @@ if __name__ == '__main__':
 # load data to dataframes
     # df_us = CleanData.load_csv(us_csv_path)
     # df_us_json = CleanData.load_json(us_json_path)
-# run the pipeline
+# run the pipeline to pickle
     # df_us = run_pipeline(df_us, df_us_json)
-    # print(df_us.columns)
     # df_us.to_pickle("../data/trends/USpipelined.pkl")
 # read pickle for faster graphing
     df = pd.read_pickle("../data/trends/USpipelined.pkl")
+    print(' --- dataframe columns and shape')
     print(df.columns)
     print(df.shape)
+# wordcloud all tags image create
+    all_tags_list = TagsResearch.create_tags_list(df)
+    all_tags_str = TagsResearch.create_str_from_list(all_tags_list)
+    # word_cloud_image(all_tags_str, "images/wc_all_standardstop.png")
 
-# Groupby by country
-country = df.groupby("country")
-
-# Summary statistic of all countries
-country.describe().head()
--- src/TagClass.py
+++ src/TagClass.py
@@ -13,8 +13,13 @@ class TagsResearch(object):
         df.loc[df['tags'] == "[none]", ['tags_count']] = 0
         return df
 
-    def create_all_tags_list(df):
+    def create_tags_list(df):
     # transform tags column into all tags df
         tags_series = pd.Series(df['tags'].str.replace('"', '').str.split('|'))
         flatten_matrix = [val for sublist in tags_series for val in sublist]
         return flatten_matrix
+
+    def create_str_from_list(lst_of_str):
+    # wordcloud wants one giant text string. so make that
+        tags_series = pd.Series(lst_of_str)
+        return tags_series.str.cat(sep=' ')
